dc5d398fdb2fba78e8272876da2fe86a902b1d53
Christian Oelsner <C.Oelsner@web.de>
2015-01-19 09:03:25 +0100

    Adding menu stub for installation purpose

--- gmediarender/gmediarender-setup.sh
+++ gmediarender/gmediarender-setup.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-./scripts/alsa-setup.sh
+title="GMediaRender Setup for Banana Pi"
+
+auswahl=$(dialog --stdout --title "$title" \
+    --checklist "What do you want to do?" 16 60 5 \
+	"ALSA" "ALSA setup" off \
+        "GStreamer" "GStreamer setup" off \
+	"Clone" "Clone the git repository" off \
+	"Make" "Make GMediaRender" off)
+
+for choice in $auswahl ; do
+  case $choice in
+    '"ASLA"')
+      ./scripts/alsa-setup.sh 
+      ;;
+    '"GStreamer"')
+      echo "GStreamer setup ..."
+      ;;
+    '"Clone"')
+      echo "Cloning ..."
+      ;;
+    '"Make"')
+      echo "Make ..."
+      ;;
+  esac
+done
